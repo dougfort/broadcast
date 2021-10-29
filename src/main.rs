a465c5b553068f0ae7b4ee4c8ef07eaeb6515e40
dougfort <doug.fort@gmail.com>
2021-10-29 12:01:41 -0400

    stubs for actors

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,30 @@ async fn main()  -> Result<(), Error> {
     }
     tracing_subscriber::fmt::init();
 
+    const ACTOR_COUNT: usize = 2;
+
     tracing::info!("program starts");
+
+    let mut join_handles = vec![];
+    for actor_id in 1..=ACTOR_COUNT {
+        let join_handle = tokio::spawn(async move {
+            actor(actor_id).await;
+        });
+        join_handles.push(join_handle);
+    }
 
     tracing::info!("waiting for shutdown signal");
     signal::shutdown_signal().await;
 
+    for join_handle in join_handles {
+        let result = join_handle.await?;
+        tracing::debug!("join result = {:?}", result);
+    }
+
     tracing::info!("progam terminates normally");
     Ok(())
+}
+
+async fn actor(actor_id: usize) {
+    tracing::debug!("actor {}", actor_id);
 }
