99f87926b6f70d5b4325197f0d322a14eba76fdd
dougfort <doug.fort@gmail.com>
2021-10-29 10:42:29 -0400

    set up tokio stub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Error;
+
+mod signal;
+
+#[tokio::main]
+async fn main()  -> Result<(), Error> {
+    // Set the RUST_LOG, if it hasn't been explicitly defined
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "broadcast=debug")
+    }
+    tracing_subscriber::fmt::init();
+
+    tracing::info!("program starts");
+
+    tracing::info!("waiting for shutdown signal");
+    signal::shutdown_signal().await;
+
+    tracing::info!("progam terminates normally");
+    Ok(())
 }
