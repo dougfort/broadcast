cf529bbc310a468bc7556bf51b3768a27a088062
dougfort <doug.fort@gmail.com>
2021-10-29 16:39:14 -0400

    add halt watch and random wait

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Error;
 use std::sync::Arc;
-use tokio::sync::broadcast;
+use tokio::sync::{broadcast, watch};
+use rand::Rng;
+use std::time::Duration;
 
 mod signal;
 
@@ -20,12 +22,16 @@ async fn main() -> Result<(), Error> {
     let (broadcast_tx, _) = broadcast::channel::<String>(BROADCAST_CHANNEL_SIZE);
     let broadcast_tx = Arc::new(broadcast_tx);
 
+    let mut halt = false;
+    let (halt_tx, halt_rx) = watch::channel(halt);
+
     let mut join_handles = vec![];
     for actor_id in 1..=ACTOR_COUNT {
         let broadcast_tx = broadcast_tx.clone();
         let broadcast_rx = broadcast_tx.subscribe();
+        let halt_rx = halt_rx.clone();
         let join_handle = tokio::spawn(async move {
-            actor(actor_id, broadcast_tx, broadcast_rx).await?;
+            actor(actor_id, broadcast_tx, broadcast_rx, halt_rx).await?;
 
             Ok::<(), Error>(())
         });
@@ -34,6 +40,9 @@ async fn main() -> Result<(), Error> {
 
     tracing::info!("waiting for shutdown signal");
     signal::shutdown_signal().await;
+
+    halt = true;
+    halt_tx.send(halt)?;
 
     for join_handle in join_handles {
         let result = join_handle.await?;
@@ -47,13 +56,26 @@ async fn main() -> Result<(), Error> {
 async fn actor(
     actor_id: usize,
     broadcast_tx: Arc<broadcast::Sender<String>>,
-    broadcast_rx: broadcast::Receiver<String>,
+    mut broadcast_rx: broadcast::Receiver<String>,
+    mut halt_rx: watch::Receiver<bool>,
 ) -> Result<(), Error> {
-    let mut broadcast_rx = broadcast_rx;
-    broadcast_tx.send(format!("1 from actor {}", actor_id))?;
-    for _ in 0..2 {
-        let data = broadcast_rx.recv().await?;
-        tracing::debug!("actor: {}; data = {}", actor_id, data);
+    let mut halt = *halt_rx.borrow(); 
+
+    while !halt {
+        let sleep_interval = {
+            let mut rng = rand::thread_rng();
+            Duration::from_secs(rng.gen_range(0..20))
+        };
+        tokio::select! {
+            _ = tokio::time::sleep(sleep_interval) => {
+                broadcast_tx.send(format!("from actor {}", actor_id))?;
+            }
+            _ = halt_rx.changed() => {halt = *halt_rx.borrow()}
+            recv_result = broadcast_rx.recv() => {
+                let data = recv_result?;
+                tracing::debug!("actor: {}; data = {}", actor_id, data);
+            }
+        }
     }
 
     tracing::debug!("actor: {} terminates", actor_id);
