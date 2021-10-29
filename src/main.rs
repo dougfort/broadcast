ce29f1feabfc2df1e296b8f4f40d875a857401ac
dougfort <doug.fort@gmail.com>
2021-10-29 13:23:06 -0400

    add stub for tokio::sync::broadcast

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Error;
+use tokio::sync::broadcast;
 
 mod signal;
 
@@ -11,13 +12,17 @@ async fn main()  -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
     const ACTOR_COUNT: usize = 2;
+    const BROADCAST_CHANNEL_SIZE: usize = ACTOR_COUNT;
 
     tracing::info!("program starts");
 
+    let (broadcast_tx, _) = broadcast::channel::<String>(BROADCAST_CHANNEL_SIZE);
+
     let mut join_handles = vec![];
     for actor_id in 1..=ACTOR_COUNT {
+        let broadcast_tx = broadcast_tx.clone();
         let join_handle = tokio::spawn(async move {
-            actor(actor_id).await;
+            actor(actor_id, broadcast_tx).await;
         });
         join_handles.push(join_handle);
     }
@@ -34,6 +39,9 @@ async fn main()  -> Result<(), Error> {
     Ok(())
 }
 
-async fn actor(actor_id: usize) {
-    tracing::debug!("actor {}", actor_id);
+async fn actor(actor_id: usize, broadcast_tx: broadcast::Sender::<String>) {
+    let mut broadcast_rx = broadcast_tx.subscribe();
+    broadcast_tx.send(format!("from actor {}", actor_id)).unwrap();
+    let data = broadcast_rx.recv().await.unwrap();
+    tracing::debug!("actor: {}; data = {}", actor_id, data);
 }
